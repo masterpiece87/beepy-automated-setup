ed3099c44129cd8d8d26ad04abcd8df2d03b4b8e
masterpiece87 <vin.amato@outlook.it>
2023-09-08 22:32:50 +0200

    changed link in beepy.list

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 KEYPATH="https://raw.githubusercontent.com/masterpiece87/beepy-ppa/master/KEY.gpg"
 PACKAGESOURCE="https://raw.githubusercontent.com/masterpiece87/beepy-ppa/master/beepy.list"
 SCHEDULED_FUNCTIONS_FOLDER=/home/scheduled
@@ -9,8 +15,8 @@ sudo curl -s --compressed -o /etc/apt/sources.list.d/beepy.list $PACKAGESOURCE
 sudo apt update
 sudo apt-get -y install beepy-kbd sharp-drm
 
+sudo mkdir $SCHEDULED_FUNCTIONS_FOLDER
 sudo wget https://raw.githubusercontent.com/masterpiece87/beepy-hacks/mainline/scripts/battery.sh -O $SCHEDULED_FUNCTIONS_FOLDER/battery.sh
-
 
 sudo echo "* * * * * (            " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
 sudo echo "* * * * * ( sleep 10 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
@@ -20,4 +26,4 @@ sudo echo "* * * * * ( sleep 40 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " 
 sudo echo "* * * * * ( sleep 50 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
 
 
-sudo reboot
+# sudo reboot
