d0966af78e6e92e685d021162dd1127f82b285c6
masterpiece87 <vin.amato@outlook.it>
2023-09-08 22:19:06 +0200

    added crontab

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 KEYPATH="https://raw.githubusercontent.com/masterpiece87/beepy-ppa/master/KEY.gpg"
 PACKAGESOURCE="https://raw.githubusercontent.com/masterpiece87/beepy-ppa/master/beepy.list"
-SCHEDULED_FUNCTIONS_FOLDER=/scheduled
+SCHEDULED_FUNCTIONS_FOLDER=/home/scheduled
 
 
 curl -s --compressed $KEYPATH | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/beepy.gpg >/dev/null
@@ -12,12 +12,12 @@ sudo apt-get -y install beepy-kbd sharp-drm
 sudo wget https://raw.githubusercontent.com/masterpiece87/beepy-hacks/mainline/scripts/battery.sh -O $SCHEDULED_FUNCTIONS_FOLDER/battery.sh
 
 
-sudo echo "* * * * * (            " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
-sudo echo "* * * * * ( sleep 10 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
-sudo echo "* * * * * ( sleep 20 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
-sudo echo "* * * * * ( sleep 30 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
-sudo echo "* * * * * ( sleep 40 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
-sudo echo "* * * * * ( sleep 50 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * (            " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * ( sleep 10 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * ( sleep 20 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * ( sleep 30 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * ( sleep 40 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * ( sleep 50 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
 
 
 sudo reboot
