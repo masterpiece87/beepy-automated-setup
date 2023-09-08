fd7f518bfefe4aaacb6a0856445c3b4b0f7b753c
masterpiece87 <vin.amato@outlook.it>
2023-09-08 22:25:29 +0200

    added battery monitor

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,12 +12,12 @@ sudo apt-get -y install beepy-kbd sharp-drm
 sudo wget https://raw.githubusercontent.com/masterpiece87/beepy-hacks/mainline/scripts/battery.sh -O $SCHEDULED_FUNCTIONS_FOLDER/battery.sh
 
 
-sudo echo "* * * * * (            " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
-sudo echo "* * * * * ( sleep 10 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
-sudo echo "* * * * * ( sleep 20 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
-sudo echo "* * * * * ( sleep 30 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
-sudo echo "* * * * * ( sleep 40 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
-sudo echo "* * * * * ( sleep 50 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /etc/cron.d/e2scrub_all
+sudo echo "* * * * * (            " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * ( sleep 10 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * ( sleep 20 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * ( sleep 30 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * ( sleep 40 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
+sudo echo "* * * * * ( sleep 50 ; " $SCHEDULED_FUNCTIONS_FOLDER "/battery.sh" " )" >> /var/spool/cron/root
 
 
 sudo reboot
